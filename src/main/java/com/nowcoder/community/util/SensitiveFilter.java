3d0313a0b51438d6cc5dc5d0d7302260819dcaf9
Shen <543190701@qq.com>
2020-05-14 11:14:36 +0800

    过滤敏感词

--- src/main/java/com/nowcoder/community/util/SensitiveFilter.java
+++ src/main/java/com/nowcoder/community/util/SensitiveFilter.java
@@ -4,6 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +21,52 @@ public class SensitiveFilter {
     //替换符
     private static final String REPLACEMENT = "***";
 
+    //根节点
+    private TrieNode rootNode = new TrieNode();
 
+    /*
+     * 被@PostConstruct修饰的方法会在服务器加载Servlet的时候运行，并且只会被服务器执行一次。
+     * PostConstruct在构造函数之后执行，init（）方法之前执行。
+     */
+    @PostConstruct
+    public void init() {
+        try (
+                InputStream is = this.getClass().getClassLoader().getResourceAsStream("sensitive-words.txt");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        ) {
+            String keyword;
+            while ((keyword = reader.readLine()) != null) {
+                //添加到前缀树
+                this.addKeyword(keyword);
+            }
+        } catch (IOException e) {
+            logger.error("加载敏感词文件失败：" + e.getMessage());
+        }
+    }
+
+    //将一个敏感词添加到前缀树中
+    private void addKeyword(String keyword) {
+        TrieNode tempNode = rootNode;
+        for (int i = 0; i < keyword.length(); i++) {
+            char c = keyword.charAt(i);
+            TrieNode subNode = tempNode.getSubNode(c);
+
+            //Trie中没有
+            if (subNode == null) {
+                //初始化节点
+                subNode = new TrieNode();
+                tempNode.addSubNode(c, subNode);
+            }
+
+            //Trie中已有，指向子节点，进入下一循环
+            tempNode = subNode;
+
+            //结束标识
+            if (i == keyword.length() - 1) {
+                tempNode.setKeywordEnd(true);
+            }
+        }
+    }
 
     //前缀树
     private class TrieNode {
