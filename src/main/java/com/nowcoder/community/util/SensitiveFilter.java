de3778ff162a0cfb0595d9ca30bd48b9c4fc100a
Shen <543190701@qq.com>
2020-05-14 10:47:23 +0800

    过滤敏感词

--- src/main/java/com/nowcoder/community/util/SensitiveFilter.java
+++ src/main/java/com/nowcoder/community/util/SensitiveFilter.java
@@ -13,6 +13,11 @@ public class SensitiveFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(SensitiveFilter.class);
 
+    //替换符
+    private static final String REPLACEMENT = "***";
+
+
+
     //前缀树
     private class TrieNode {
 
@@ -22,6 +27,22 @@ public class SensitiveFilter {
         // 子节点 (key为下级字符，value为下级节点)
         private Map<Character, TrieNode> subNodes = new HashMap<>();
 
+        public boolean isKeywordEnd() {
+            return isKeywordEnd;
+        }
 
+        public void setKeywordEnd(boolean keywordEnd) {
+            isKeywordEnd = keywordEnd;
+        }
+
+        //添加子节点
+        public void addSubNode(Character c, TrieNode node) {
+            subNodes.put(c, node);
+        }
+
+        //获取子节点
+        public TrieNode getSubNode(Character c) {
+            return subNodes.get(c);
+        }
     }
 }
