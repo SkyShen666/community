1b124a07edfc6a09c10ec5b52b7889c8fa38682f
Shen <543190701@qq.com>
2020-05-18 00:16:27 +0800

    显示评论

--- src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -109,12 +109,14 @@ public class DiscussPostController implements CommunityConstant {
                 commentVo.put("replys", replyVoList);
 
                 //回复数量
-                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId())
-                commentVo.put("replyCount",)
+                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVo.put("replyCount",replyCount);
 
                 commentVoList.add(commentVo);
             }
         }
+
+        model.addAttribute("comments", commentVoList);
 
         return "/site/discuss-detail";
     }
