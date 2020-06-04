86d6d7f1c2e3de4d151e7b2234350595bb396b9c
Shen <543190701@qq.com>
2020-06-04 22:23:53 +0800

    点赞

--- src/main/java/com/nowcoder/community/controller/LikeController.java
+++ src/main/java/com/nowcoder/community/controller/LikeController.java
@@ -5,12 +5,15 @@ import com.nowcoder.community.service.LikeService;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Controller
 public class LikeController {
 
     @Autowired
@@ -20,6 +23,7 @@ public class LikeController {
     private HostHolder hostHolder;
 
     @RequestMapping(path = "/like", method = RequestMethod.POST)
+    @ResponseBody
     public String like(int entityType, int entityId, int entityUserId) {
         User user = hostHolder.getUser();
 
