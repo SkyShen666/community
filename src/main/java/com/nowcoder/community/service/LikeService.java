253a13aa10394a962427e3cfe62ffafd83c5b6b4
Shen <543190701@qq.com>
2020-06-05 17:07:35 +0800

    我收到的赞

--- src/main/java/com/nowcoder/community/service/LikeService.java
+++ src/main/java/com/nowcoder/community/service/LikeService.java
@@ -14,7 +14,14 @@ public class LikeService {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    // 点赞
+    /**
+     * 点赞
+     * @param userId 点赞的人
+     * @param entityType 被赞的实体类型
+     * @param entityId 被赞的实体id
+     * @param entityUserId 实体作者的id
+     * 此处为了不访问数据库（访问数据库会让性能降低）
+     */
     public void like(int userId, int entityType, int entityId, int entityUserId) {
         redisTemplate.execute(new SessionCallback() {
             @Override
@@ -24,8 +31,15 @@ public class LikeService {
 
                 boolean isMember = operations.opsForSet().isMember(entityLikeKey, userId);
 
+                // 开启事务
                 operations.multi();
 
+                /**
+                 * 来判断用户点没点过赞，此查询一定要放在事务的过程之外
+                 * redis事务比较特殊，若把查询写在查询过程之内，不会立刻得到结果，
+                 * 因为它在事务执行过程之中，你所有的命令没有立刻执行，
+                 * 而是把这些命令放在了队列里，当你提交事务时会统一提交， 再执行。
+                 */
                 if (isMember) {
                     operations.opsForSet().remove(entityLikeKey, userId);
                     operations.opsForValue().decrement(userLikeKey);
@@ -34,6 +48,7 @@ public class LikeService {
                     operations.opsForValue().increment(userLikeKey);
                 }
 
+                // 执行事务
                 return operations.exec();
             }
         });
