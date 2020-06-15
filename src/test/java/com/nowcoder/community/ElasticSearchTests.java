b7948d9db02f25462aec0b0a99f44494a153eeb1
Shen <543190701@qq.com>
2020-06-15 18:12:46 +0800

    elasticsearchTest

---
+++ src/test/java/com/nowcoder/community/ElasticSearchTests.java
@@ -0,0 +1,96 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class ElasticSearchTests {
+    @Autowired
+    private DiscussPostMapper discussMapper;
+
+    @Autowired
+    private DiscussPostRepository discussRepository;
+
+    @Autowired
+    private ElasticsearchTemplate elasticTemplate;
+
+    @Test
+    public void testInsert() {
+        discussRepository.save(discussMapper.selectDiscussPostById(241));
+        discussRepository.save(discussMapper.selectDiscussPostById(242));
+        discussRepository.save(discussMapper.selectDiscussPostById(243));
+    }
+
+    @Test
+    public void testInsertList() {
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(101, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(102, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(103, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(111, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(112, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(131, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(132, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(133, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(134, 0, 100));
+    }
+
+    @Test
+    public void testUpdate() {
+        DiscussPost post = discussMapper.selectDiscussPostById(231);
+        post.setContent("我是大水比，使劲灌水！");
+        discussRepository.save(post);
+    }
+
+    @Test
+    public void testDelete() {
+//        discussRepository.deleteById(231);
+        discussRepository.deleteAll();
+    }
+
+    @Test
+    public void testSearchByRepository() {
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery("互联网寒冬", "title", "content"))
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("status").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(0, 10))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+
+        // elasticTemplate.queryForPage(searchQuery, class, SearchResultMapper)
+        // 底层获取得到了高亮显示的值, 但是没有返回.
+
+        Page<DiscussPost> page = discussRepository.search(searchQuery);
+        System.out.println("共有多少条数据:" + page.getTotalElements());
+        System.out.println("共多少页:" + page.getTotalPages());
+        System.out.println("当前是第多少页:" + page.getNumber());
+        System.out.println("每个页的大小:" + page.getSize());
+        int k = 0;
+        for (DiscussPost post : page) {
+            System.out.println(post);
+            System.out.println("============第" + k + "条=================");
+            k++;
+        }
+    }
+}
