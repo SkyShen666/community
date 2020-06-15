00146a4da1d3ec7e4f5a3f00996953fa370f7c2d
Shen <543190701@qq.com>
2020-06-15 23:53:20 +0800

    搜索功能

---
+++ src/main/java/com/nowcoder/community/service/ElasticsearchService.java
@@ -0,0 +1,116 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ElasticsearchService {
+
+    @Autowired
+    private DiscussPostRepository discussRepository;
+
+    @Autowired
+    private ElasticsearchTemplate elasticTemplate;
+
+    public void saveDiscussPost(DiscussPost post) {
+        discussRepository.save(post);
+    }
+
+    public void deleteDiscussPost(int id) {
+        discussRepository.deleteById(id);
+    }
+
+    public Page<DiscussPost> searchDiscussPost(String keyword, int current, int limit) {
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery(keyword, "title", "content"))
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("status").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(current, limit))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+
+        return elasticTemplate.queryForPage(searchQuery, DiscussPost.class, new SearchResultMapper() {
+            @Override
+            public <T> AggregatedPage<T> mapResults(SearchResponse searchResponse, Class<T> aClass, Pageable pageable) {
+                SearchHits hits = searchResponse.getHits();
+                if (hits.getTotalHits() <= 0) {
+                    return null;
+                }
+
+                List<DiscussPost> list = new ArrayList<>();
+                for (SearchHit hit : hits) {
+                    DiscussPost discussPost = new DiscussPost();
+
+                    String id = hit.getSourceAsMap().get("id").toString();
+                    discussPost.setId(Integer.valueOf(id));
+
+                    String userId = hit.getSourceAsMap().get("userId").toString();
+                    discussPost.setUserId(Integer.valueOf(userId));
+
+                    String title = hit.getSourceAsMap().get("title").toString();
+                    discussPost.setTitle(title);
+
+                    String content = hit.getSourceAsMap().get("content").toString();
+                    discussPost.setTitle(content);
+
+                    String type = hit.getSourceAsMap().get("type").toString();
+                    discussPost.setId(Integer.valueOf(type));
+
+                    String status = hit.getSourceAsMap().get("status").toString();
+                    discussPost.setId(Integer.valueOf(status));
+
+                    String createTime = hit.getSourceAsMap().get("createTime").toString();
+                    discussPost.setCreateTime(new Date(Long.valueOf(createTime)));
+
+                    String commentCount = hit.getSourceAsMap().get("commentCount").toString();
+                    discussPost.setId(Integer.valueOf(commentCount));
+
+                    // 处理高亮显示的结果
+                    HighlightField titleField = hit.getHighlightFields().get("title");
+                    if (titleField != null) {
+                        // 处理第一个和title匹配到的词就可以，随意用getFragments()[0]
+                        discussPost.setTitle(titleField.getFragments()[0].toString());
+                    }
+
+                    HighlightField contentField = hit.getHighlightFields().get("content");
+                    if (contentField != null) {
+                        discussPost.setContent(contentField.getFragments()[0].toString());
+                    }
+
+                    list.add(discussPost);
+                }
+                return new AggregatedPageImpl(list, pageable,
+                        hits.getTotalHits(), searchResponse.getAggregations(), searchResponse.getScrollId(), hits.getMaxScore());
+            }
+        });
+    }
+
+
+}
+
