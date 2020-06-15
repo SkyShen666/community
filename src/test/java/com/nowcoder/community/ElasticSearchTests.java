b29d024ba2f3035c435126a0bd2bbd379f36041c
Shen <543190701@qq.com>
2020-06-15 21:07:59 +0800

    elasticsearchTest

--- src/test/java/com/nowcoder/community/ElasticSearchTests.java
+++ src/test/java/com/nowcoder/community/ElasticSearchTests.java
@@ -3,8 +3,14 @@ package com.nowcoder.community;
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
 import com.nowcoder.community.entity.DiscussPost;
+import javafx.scene.control.TableColumn;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortBuilder;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
@@ -13,11 +19,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -82,6 +96,86 @@ public class ElasticSearchTests {
         // 底层获取得到了高亮显示的值, 但是没有返回.
 
         Page<DiscussPost> page = discussRepository.search(searchQuery);
+        System.out.println("共有多少条数据:" + page.getTotalElements());
+        System.out.println("共多少页:" + page.getTotalPages());
+        System.out.println("当前是第多少页:" + page.getNumber());
+        System.out.println("每个页的大小:" + page.getSize());
+        int k = 0;
+        for (DiscussPost post : page) {
+            System.out.println(post);
+            System.out.println("============第" + k + "条=================");
+            k++;
+        }
+    }
+
+    @Test
+    public void testSearchByTemplate() {
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery("互联网寒冬", "title", "content"))
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("status").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(0, 10))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+
+        Page<DiscussPost> page = elasticTemplate.queryForPage(searchQuery, DiscussPost.class, new SearchResultMapper() {
+            @Override
+            public <T> AggregatedPage<T> mapResults(SearchResponse searchResponse, Class<T> aClass, Pageable pageable) {
+                SearchHits hits = searchResponse.getHits();
+                if (hits.getTotalHits() <= 0) {
+                    return null;
+                }
+
+                List<DiscussPost> list = new ArrayList<>();
+                for (SearchHit hit : hits) {
+                    DiscussPost discussPost = new DiscussPost();
+
+                    String id = hit.getSourceAsMap().get("id").toString();
+                    discussPost.setId(Integer.valueOf(id));
+
+                    String userId = hit.getSourceAsMap().get("userId").toString();
+                    discussPost.setUserId(Integer.valueOf(userId));
+
+                    String title = hit.getSourceAsMap().get("title").toString();
+                    discussPost.setTitle(title);
+
+                    String content = hit.getSourceAsMap().get("content").toString();
+                    discussPost.setTitle(content);
+
+                    String type = hit.getSourceAsMap().get("type").toString();
+                    discussPost.setId(Integer.valueOf(type));
+
+                    String status = hit.getSourceAsMap().get("status").toString();
+                    discussPost.setId(Integer.valueOf(status));
+
+                    String createTime = hit.getSourceAsMap().get("createTime").toString();
+                    discussPost.setCreateTime(new Date(Long.valueOf(createTime)));
+
+                    String commentCount = hit.getSourceAsMap().get("commentCount").toString();
+                    discussPost.setId(Integer.valueOf(commentCount));
+
+                    // 处理高亮显示的结果
+                    HighlightField titleField = hit.getHighlightFields().get("title");
+                    if (titleField != null) {
+                        // 处理第一个和title匹配到的词就可以，随意用getFragments()[0]
+                        discussPost.setTitle(titleField.getFragments()[0].toString());
+                    }
+
+                    HighlightField contentField = hit.getHighlightFields().get("content");
+                    if (contentField != null) {
+                        discussPost.setContent(contentField.getFragments()[0].toString());
+                    }
+
+                    list.add(discussPost);
+                }
+                return new AggregatedPageImpl(list, pageable,
+                        hits.getTotalHits(), searchResponse.getAggregations(), searchResponse.getScrollId(), hits.getMaxScore());
+            }
+        });
+
         System.out.println("共有多少条数据:" + page.getTotalElements());
         System.out.println("共多少页:" + page.getTotalPages());
         System.out.println("当前是第多少页:" + page.getNumber());
